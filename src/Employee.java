e901293a12bd541b0bd268be6db45570168594b1
Rory Harford <Roryharford@live.ie>
2019-03-13 18:16:02 +0000

    Values will be blank anyway unnecessary to set them to blank values in the Employee class

--- src/Employee.java
+++ src/Employee.java
@@ -13,19 +13,9 @@ public class Employee{
 	private String department;
 	private double salary;
 	private boolean fullTime;
-
-	//potentially get rid
-	// Create Employee with no details
+	
 	public Employee() {
-		this.employeeId = 0;
-		this.pps = "";
-		this.surname = "";
-		this.firstName = "";
-		this.gender = '\0';
-		this.department = "";
-		this.salary = 0;
-		this.fullTime = false;
-	}//end Employee with no details
+	}
 
 	// Create Employee with details
 	public Employee(int employeeId, String pps, String surname, String firstName, char gender, String department, double salary,
